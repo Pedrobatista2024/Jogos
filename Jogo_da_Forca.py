8b479b09b9a3c5f87df10103e68ad846e4377f99
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-07-02 22:55:50 -0300

    forca versão03

--- Jogo_da_Forca.py
+++ Jogo_da_Forca.py
@@ -22,3 +22,21 @@ def game():
     chances = 6
 
     letras_erradas = []
+
+    while chances > 0:
+
+        print(" ".join(letras_descobertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas:", " ".join(letras_erradas))
+
+        tentativa = input("\nDigite uma letra: ").lower()
+
+        if tentativa in palavra:
+            index = 0
+            for letra in palavra:
+                if tentativa == letra:
+                    letras_descobertas[index] = letra
+                index += 1
+        else:
+            chances -= 1
+            letras_erradas.append(tentativa)
