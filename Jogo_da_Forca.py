1750f22269f379d933b01fa51179aa4c4c116e55
Pedro Batista Neto <pedroplpellegrinni1918@gmail.com>
2025-07-02 22:30:45 -0300

    forca versão02

--- Jogo_da_Forca.py
+++ Jogo_da_Forca.py
@@ -1,8 +1,24 @@
 import random
 from os import system, name
 
+
 def limpa_tela():
     if name == 'nt':
         _= system('cls')
     else:
         _= system("clear")
+
+def game():
+    limpa_tela()
+    print('\nBem-vindo(a) ao jogo da forca!')
+    print('Adivinhe a palavra abaixo')
+
+    palavras = ['banana','abacate','uva','morango','laranja']
+
+    palavra = random.choice(palavras)
+
+    letras_descobertas = ['_' for letra in palavra]
+
+    chances = 6
+
+    letras_erradas = []
